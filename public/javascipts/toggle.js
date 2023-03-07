baef2b98cde4b6608208137e8eba5d512e8588df
Khoi <dduongthang0702@gmail.com>
2023-03-07 14:44:41 +0800

    uploaded homePage second

--- public/javascipts/toggle.js
+++ public/javascipts/toggle.js
@@ -1,6 +1,22 @@
 const body = document.querySelector('body');
 const toggle = document.getElementById('toggle');
-toggle.onclick = function () {
+// const navs = document.querySelector('.nav li a');
+// const navS = document.querySelectorAll('.nav-link');
+const headerElement = document.querySelector('#header');
+const introContent = document.querySelector('.intro-content');
+const featureContent = document.querySelector('.feature-content');
+const developerContent = document.querySelector('#developer p');
+const bowlElement = document.querySelector('.bowl');
+
+toggle.onclick = () => {
     toggle.classList.toggle('active');
     body.classList.toggle('active');
+    headerElement.classList.toggle('active');
+    // navs.classList.toggle('active');
+    // navS.classList.toggle('active');
+    introContent.classList.toggle('active');
+    featureContent.classList.toggle('active');
+    developerContent.classList.toggle('active');
+    bowlElement.classList.toggle('active');
+
 }
